0766a4662250e2f115706c498591cff80c0a62c6
ThomassMathisen <thomasmathisen@hotmail.com>
2024-01-12 16:43:24 +0100

    Design single post page

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,44 @@
-import "./singlePost.module.css";
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+import { Suspense } from "react";
 
-const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+const SinglePostPage = ({ params }) => {
+  const { slug } = params;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/noavatar.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <div>
+            <Image
+              src="/noavatar.png"
+              alt=""
+              width={50}
+              height={50}
+              className={styles.avatar}
+            />
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Jack Black</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis a
+          numquam laboriosam. Aperiam quod eius ipsam qui earum! Sequi odit eos
+          ea atque blanditiis nesciunt, adipisci quia iste voluptatibus
+          voluptas.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
